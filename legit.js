f9f6042a99b0229d43f96c9cad7faa089c65e4d6
Vijay Rudraraju <melbournevijay@gmail.com>
2014-12-19 20:01:30 +1100

    Updates

--- legit.js
+++ legit.js
@@ -8,9 +8,20 @@ function _(selector){
 
 	/* BASIC FUNCTIONS */
 
-	//grab the html of an element mz('selector').html()
-	self.html = function(){
-		return self.element;
+	//get the innerhtml of an element mz('selector').html()
+	self.html = function(value){
+		if(!value) return self.element;
+
+		self.element.innerHTML = value;
+		return self;
+	}
+	//get and set attribute values
+	self.attr = function(name,value){
+		//if no value set, return the current value
+		if(!value) return self.element.getAttribute(name);
+
+		self.element.setAttribute(name,value);
+		return self;
 	}
 	//get the height of an element
 	self.height = function(){
@@ -31,14 +42,7 @@ function _(selector){
 		self.element = self.element.childNodes[key];
 		return self;
 	}
-	//get and set attribute values
-	self.attr = function(name,value){
-		//if no value set, return the current value
-		if(!value) return self.element.getAttribute(name);
-
-		self.element.setAttribute(name,value);
-		return self;
-	}
+	
 
 	/* Event FUNCTIONS */
 
@@ -76,6 +80,67 @@ function _(selector){
 
 		self.element.style[name] = value;
 		return self;
+	}
+
+
+  /* Additional function thanks to http://youmightnotneedjquery.com/ */
+  self.text = function(value){ //get the textContent of an element mz('selector').text()	
+		if(!value) return self.element.textContent;
+		self.element.textContent = value;
+		return self;
+	}
+	self.hasClass = function(className){ //check if element has class
+		if (self.element.classList) self.element.classList.contains(className);
+		else new RegExp('(^| )' + className + '( |$)', 'gi').test(self.element.className);
+    return self;
+	}
+  self.addClass = function(className){ //add a class
+		if (self.element.classList) self.element.classList.add(className);
+		else self.element.className += ' ' + className;
+    return self;
+	}
+  self.removeClass = function(className){ //remove a class
+		if (self.element.classList) self.element.classList.remove(className);
+		else self.element.className = self.element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    return self;
+	}
+	self.toggleClass = function(className){ //toggle a class
+		if (self.element.classList) { self.element.classList.toggle(className);
+		} else {
+		  var classes = self.element.className.split(' ');
+		  var existingIndex = classes.indexOf(className);
+		  if (existingIndex >= 0) classes.splice(existingIndex, 1);
+		  else classes.push(className);
+       self.element.className = classes.join(' ');
+		}
+    return self;
+	}
+	self.next = function(){ //get next element
+		self.element = self.element.nextElementSibling;
+		return self;
+	}
+	self.prev = function(){ //get previous element
+		self.element = self.element.previousElementSibling;
+		return self;
+	}
+	self.hide = function(){ //hide an element
+		self.element.style.display = 'none';
+		return self;
+	}
+	self.show = function(){ //show an element
+		self.element.style.display = '';
+		return self;
+	}
+	self.fadeIn = function(element){ //fadein an element
+    self.element.style.opacity = 0;
+    var last = +new Date();
+	  var tick = function() {
+	    self.element.style.opacity = +self.element.style.opacity + (new Date() - last) / 400;
+	    last = +new Date();
+	    if (+self.element.style.opacity < 1) { (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16) }
+	  };
+	  tick();
+		return self;  
 	}
 
 	return self;
